8a111467c632beaefa2926e0a5e24941bd043ee3
tech-kartik08 <techgju2023@gmail.com>
2025-07-30 15:22:39 +0530

    commit

--- flask_tut/app.py
+++ flask_tut/app.py
@@ -9,7 +9,6 @@ uri=os.getenv("ASS_URI")
 
 # connect mongodb
 
-
 client = MongoClient(uri)
 try:
     client.admin.command('ping')
@@ -22,21 +21,29 @@ db = client['assignment-db']
 collection = db['ass-data']
 
 # flask application
-
 app = Flask(__name__)
 
+# home page
 @app.route('/')
 def home():
 
     return render_template('index.html')
 
+# after submission
 @app.route('/submit' , methods=['POST'])
 def submit():
+
     form_data = dict(request.form)
+    
+    if not form_data.get('name') or not form_data.get('email'):
+        error='Name and email are required'
+        return render_template('index.html', error=error,form_data=form_data)
+
     collection.insert_one(form_data)
     name = form_data.get('name','user')
     return f"Hello {name} , your data has been successfuly submitted"
 
+# get data
 @app.route('/api')
 def get_data():
     data = list(collection.find({},{'_id': 0}))
